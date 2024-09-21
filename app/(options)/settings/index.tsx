282b3d262ef72c4e0e5accf566dfa43fe10d1f97
Vandrei P Ribeiro <vandreiribeiro@gmail.com>
2024-09-20 21:15:20 -0300

    update

--- app/(options)/settings/index.tsx
+++ app/(options)/settings/index.tsx
@@ -1,18 +1,48 @@
-import { StyleSheet, Text } from "react-native";
+import { Button, SafeAreaView, StyleSheet, Text, useColorScheme } from "react-native";
 import Icon from "@expo/vector-icons/Ionicons";
+import { useState } from "react";
 
 export default function Settings() {
+    const [theme, setTheme] = useState("light")
+
+    function handleTheme() {
+        if (theme == "light") {
+            setTheme("dark")
+        } else {
+            setTheme("light")
+        }
+    }
+
     return (
-        <>
-            <Text style={styles.fontText}>É a pagina de Settings</Text>
+        <SafeAreaView style={theme == "light" ? styles.lightTheme : styles.darkTheme}>
+            <Text style={theme == "light" ? styles.lightThemeText : styles.darkThemeText}>É a pagina de Settings</Text>
             <Icon name="airplane" size={64} color="green" />
-        </>
+            <Button title="MUDA DE TEMA" onPress={handleTheme} />
+        </SafeAreaView>
     )
+
+
 }
 
 const styles = StyleSheet.create({
     fontText: {
         fontFamily: 'SofadiOne',
-        fontSize: 52
+        fontSize: 52,
+    },
+    darkTheme: {
+        backgroundColor: "#000",
+    },
+    lightTheme: {
+        backgroundColor: "#FFF",
+    },
+    darkThemeText: {
+        color: "#FFF",
+        fontFamily: 'SofadiOne',
+        fontSize: 52,
+    },
+    lightThemeText: {
+        color: "#000",
+        fontFamily: 'SofadiOne',
+        fontSize: 52,
     }
 })
